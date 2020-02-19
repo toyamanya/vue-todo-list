ad9f27c0b1fc0a77b8d3e1c6f4347e18c6ed015b
toyamanya <ryotoyamaiii@gmail.com>
2020-02-19 16:20:24 +0900

    add ItemList

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,22 @@
   <div>
     <h1>{{ msg }}</h1>
     <counter />
+    <item-list />
   </div>
 </template>
 
 <script>
 import Counter from "./Counter.vue";
+import ItemList from "./ItemList.vue";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
   },
   components: {
-    Counter
+    Counter,
+    ItemList
   }
 };
 </script>
-
-<style lang="stylus"></style>
